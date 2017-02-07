f27ea7941fc916dfbd5f18afdd1fca8328b46fd7
sametKaust <samet.guler@kaust.edu.sa>
2017-02-07 05:48:30 +0300

    State machine

---
+++ state_machine/src/main.cpp
@@ -0,0 +1,68 @@
+#include <ros/ros.h>
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include "geometry_msgs/Twist.h"
+#include <sensor_msgs/image_encodings.h>
+//OpenCV related classes
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+//Vision classes
+#include "ModeSelect.cpp"
+#include "CascadeDet.cpp"
+#include "ValveDet.cpp"
+
+
+using namespace std;
+using namespace cv;
+
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "image_processor");
+	ros::NodeHandle node_Sm;
+
+	//Mode select class
+	ModeSelect modeObj;
+	int mode_State = 0;
+
+	//Cascade detection object
+	CascadeDet cascadeObj;
+	cascadeObj.trainNN();
+
+    //Valve detection object
+    ValveDet valveObj;
+
+
+    //Publish/Subscribe
+    image_transport::ImageTransport it(node_Sm);
+	image_transport::Subscriber sub = it.subscribe("usb_cam/image_raw", 1, &CascadeDet::imageCallback, &cascadeObj);
+	ros::Publisher pixelDiff_pub = node_Sm.advertise<geometry_msgs::Twist>("pixelDiff", 1000);
+    
+
+	ros::Rate r(5);
+	while(ros::ok())
+	{
+		mode_State = modeObj.modeSwitch(valveObj.radiusValve);
+		switch(mode_State){
+			case 0:
+			valveObj.detect_valve(cascadeObj.frame, cascadeObj.frame_gray);
+			case 1:
+			cascadeObj.detectProcess();
+		}
+
+		//Publish variables to UR5
+		pixelDiff_pub.publish(cascadeObj.xyzPxMsg);
+		cout << "MAIN: pixelDiff: x[" << cascadeObj.xyzPxMsg.linear.x << "]  y[" << cascadeObj.xyzPxMsg.linear.y <<"]" << endl;
+        cout << "MAIN: mode: " << mode_State << endl;
+        ros::spinOnce();
+		r.sleep();
+	}
+
+	cv::destroyAllWindows();
+	ROS_INFO("State_machine::main.cpp::No error.");
+}
